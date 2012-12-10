863810979cedf19e34f4fee011c07ef2d1669f41
peterzd <peterzd@gmail.com>
2012-12-10 22:58:35 +0800

    start to refactor the code in game.rb

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -11,22 +11,30 @@ module Codebreaker
 		end
 
 		def guess(guess)
-			mark = ''
-			(0..3).each do |index|
-				if exact_match?(guess, index)
-					mark << '+'
-				end
-			end
-
-			(0..3).each do |index|
-				if number_match?(guess, index)
-					mark << '-'
-				end
-			end
-			@output.puts mark
+			@output.puts '+' * exact_match_count(guess) + '-' * number_match_count(guess)
 		end
 
 		private
+		def exact_match_count(guess)
+			exact_match_count = 0
+			(0..3).each do |index|
+				if exact_match?(guess, index)
+					exact_match_count += 1
+				end
+			end
+			exact_match_count
+		end
+
+		def number_match_count(guess)
+			number_match_count = 0
+			(0..3).each do |index|
+				if number_match?(guess, index)
+					number_match_count += 1
+				end
+			end
+			number_match_count
+		end
+
 		def exact_match?(guess, index)
 			guess[index] == @secret[index]
 		end
