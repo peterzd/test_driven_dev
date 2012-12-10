f4920c3fa676b725de3026798c73c080ed69fed6
peterzd <peterzd@gmail.com>
2012-12-10 12:34:22 +0800

    finish chapter 4 .

--- codebreaker/features/step_definitions/codebreaker_steps.rb
+++ codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -1,2 +1,24 @@
+class Output
+	def messages
+		@messages ||= []
+	end
+	def puts(message)
+		messages << message
+	end
+end
+
+def output
+	@output ||= Output.new
+end
+
 Given /^I am not yet playing$/ do
+end
+
+When /^I start a new game$/ do
+	game = Codebreaker::Game.new(output)
+	game.start
+end
+
+Then /^I shold see "(.*?)"$/ do |message|
+	output.messages.should include(message)
 end
