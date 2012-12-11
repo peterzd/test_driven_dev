d59d4335f7da557e0e670ed36304a528a820c1fe
peterzd.work <zdsunshine0640@126.com>
2012-12-11 18:01:03 +0800

    try to think about exploratiry testing. Another circle

--- codebreaker/lib/codebreaker/marker.rb
+++ codebreaker/lib/codebreaker/marker.rb
@@ -10,9 +10,7 @@ class Marker
 	end
 
 	def number_match_count
-		(0..3).inject(0) do |count, index|
-			count + (number_match?(index) ? 1 : 0)
-		end
+		total_match_count - exact_match_count
 	end
 
 	def exact_match?(index)
@@ -21,5 +19,17 @@ class Marker
 
 	def number_match?(index)
 		@secret.include?(@guess[index]) && !exact_match?(index)
+	end
+
+	def total_match_count
+		count = 0
+		secret = @secret.split('')
+		@guess.split('').map do |n|
+			if secret.include?(n)
+				secret.delete_at(secret.index(n))
+				count += 1
+			end
+		end
+		count
 	end
 end
